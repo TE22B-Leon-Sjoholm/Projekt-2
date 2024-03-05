5b6d18dfc0e861d860e1db41a00c9deb58afa73f
Leon Sjöholm <leon.sjoholm@elev.ga.ntig.se>
2024-03-05 14:43:30 +0100

    fixat js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
-let kundvagn = JSON.parse(localStorage.getItem("kundvagn")) || [];
-console.log(kundvagn)
+let kundvagn = [];
 
 document.addEventListener("DOMContentLoaded", (event) => {
   if (localStorage.getItem("kundvagn")) {
@@ -14,10 +13,16 @@ document.addEventListener("DOMContentLoaded", (event) => {
 function växlaKundvagn() {
   var kundvagnVäxla = document.getElementById("kundvagn");
   kundvagnVäxla.classList.toggle("kundvagn-synlig");
+  document.querySelector("#varor").innerHTML = ""
+  kundvagn.forEach(vara => {
+    document.querySelector("#varor").innerHTML += "<li>" + vara.vara + " - " + vara.pris + "kr</li>";
+  })
 }
 
 function addKundvagn(vara, pris) {
+  console.log(kundvagn)
   kundvagn.push({vara: vara, pris: pris});
+  document.querySelector("#varor").innerHTML += "<li>" + vara + " - " + pris + "kr</li>";
   localStorage.setItem("kundvagn", JSON.stringify(kundvagn));
   console.log(vara, pris);
 }
